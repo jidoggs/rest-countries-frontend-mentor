fe5cf37dcb373bae5d4e1d199f3d45e9e5c2b5fd
jidoggs <jidoggs@gmail.com>
2022-01-28 16:01:03 +0100

    Routes and  system structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/HOC/Header";
+import Profile from "./components/Pages/Profile";
+import Home from "./components/Pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:countyName" element={<Profile />} />
+      </Routes>
+    </Header>
   );
 }
 
