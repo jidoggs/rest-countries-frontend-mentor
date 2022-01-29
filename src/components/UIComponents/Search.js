1012dddcede7317419f14e7c94d0683efa2da498
jidoggs <jidoggs@gmail.com>
2022-01-29 08:40:26 +0100

    Filter by input

--- src/components/UIComponents/Search.js
+++ src/components/UIComponents/Search.js
@@ -1,13 +1,20 @@
 import React from "react";
+import SearchIcon from "../../static/images/SearchIcon";
 
 function Search({ value, setValue }) {
   return (
-    <form>
-      <label htmlFor="countryInputFilter"></label>
+    <form className="search__form">
+      <label
+        htmlFor="countryInputFilter"
+        className="search__input--label"
+      ></label>
+      <SearchIcon />
       <input
+        className="search__input"
         type="search"
         name="countryInputFilter"
         id="countryInputFilter"
+        placeholder="Search for a country..."
         value={value}
         onChange={(e) => setValue(e.target.value)}
       />
